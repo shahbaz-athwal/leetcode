58ffc35c99358a633bf858fab7d8beb6b96e2d9d
shahbaz-athwal <shahbazathwal2107@gmail.com>
2025-08-11 04:08:04 -0600

    236 ✅

---
+++ typescript/236-lowest-common-ancestor-of-a-binary-tree.ts
@@ -0,0 +1,86 @@
+// Created by shahbaz_athwal at 2025/08/11 03:33
+// leetgo: 1.4.15
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+/*
+236. Lowest Common Ancestor of a Binary Tree (Medium)
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the [definition of LCA on
+Wikipedia](https://en.wikipedia.org/wiki/Lowest_common_ancestor): “The lowest common ancestor is
+defined between two nodes `p` and `q` as the lowest node in `T` that has both `p` and `q` as
+descendants (where we allow **a node to be a descendant of itself**).”
+
+**Example 1:**
+
+![](https://assets.leetcode.com/uploads/2018/12/14/binarytree.png)
+
+```
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+```
+
+**Example 2:**
+
+![](https://assets.leetcode.com/uploads/2018/12/14/binarytree.png)
+
+```
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to
+the LCA definition.
+```
+
+**Example 3:**
+
+```
+Input: root = [1,2], p = 1, q = 2
+Output: 1
+```
+
+**Constraints:**
+
+- The number of nodes in the tree is in the range `[2, 10⁵]`.
+- `-10⁹ <= Node.val <= 10⁹`
+- All `Node.val` are **unique**.
+- `p != q`
+- `p` and `q` will exist in the tree.
+
+*/
+
+// TreeNode class for binary tree problems
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+// @lc code=begin
+
+function lowestCommonAncestor(
+  root: TreeNode | null,
+  p: TreeNode | null,
+  q: TreeNode | null
+): TreeNode | null {
+  function helper(root: TreeNode | null): TreeNode | null {
+    if (root === null) return null;
+    if (root === p || root === q) return root;
+
+    const left = helper(root.left);
+    const right = helper(root.right);
+
+    if (left && right) return root;
+
+    return left || right;
+  }
+
+  return helper(root);
+}
+
+// @lc code=end
